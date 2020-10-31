c6cd385371729ba16e5e6c6e2aae1971723be0d0
Mykhailo Chui <mihach0493@gmail>
2020-10-31 14:08:14 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+
+import Crypto from './Crypto';
+// import CryptoList from './CryptoList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Crypto Rate</h1>
+      </header>
+      <Crypto />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Crypto.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import './Crypto.css';
+
+import axios from 'axios';
+import CryptoList from './CryptoList';
+class Crypto extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cryptoList: [],
+            filteredCryptoList: [],
+        }
+    }
+
+    componentDidMount() {
+        this.getCryptoDate();
+        this.timerID = setInterval(() => this.getCryptoDate(), 5000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    getCryptoDate = () => {
+
+        axios.get('https://blockchain.info/pl/ticker')
+            .then(res => {
+                const tickers = res.data;
+
+
+
+                this.setState((state) => {
+                    let newCryptoList = [];
+                    for (const [ticker, cryptoRate] of Object.entries(tickers)) {
+                        let lastCryptoObj = state.cryptoList.find((cryptoObj) => {
+                            return (cryptoObj.currency === ticker);
+                        });
+
+                        // console.log(lastCryptoObj);
+
+
+                        let newCryptoObj = {
+                            currency: ticker, symbol: cryptoRate.symbol,
+                            buy: cryptoRate.buy,
+                            sell: cryptoRate.sell,
+                            lastRate: cryptoRate.last,
+                        }
+
+                        if (lastCryptoObj !== undefined) {
+                            if (newCryptoObj.lastRate > lastCryptoObj.lastRate) {
+                                newCryptoObj.cssClass = 'green';
+                                newCryptoObj.htmlArray = String.fromCharCode(8593);
+                            } else if (newCryptoObj.lastRate < lastCryptoObj.lastRate) {
+                                newCryptoObj.cssClass = 'red';
+                                newCryptoObj.htmlArray = String.fromCharCode(8595);
+                            } else {
+                                newCryptoObj.cssClass = 'blue';
+                                newCryptoObj.htmlArray = String.fromCharCode(8596);
+                            }
+
+
+
+                        } else {
+                            newCryptoObj.cssClass = 'blue';
+                            newCryptoObj.htmlArray = String.fromCharCode(8596);
+                        }
+
+                        newCryptoList.push(newCryptoObj);
+                    }
+                    return ({
+                        cryptoList: newCryptoList
+                    })
+                });
+
+                this.filterCryptoList();
+                // console.log(res.data);
+            });
+    }
+
+    filterCryptoList = () => {
+        this._inputFilter.value = this._inputFilter.value.trim().toUpperCase();
+
+
+
+        this.setState((state) => {
+            let newFilteredCryptoList = state.cryptoList.filter((cryptoObj) => {
+                return (cryptoObj.currency.includes(this._inputFilter.value));
+            });
+
+
+            return({
+                filteredCryptoList:  newFilteredCryptoList
+            });
+        });
+        // console.log(this._inputFilter.value);
+    }
+
+
+
+    render() {
+        return (
+            <div className="Crypto">
+                <input ref={(element) => { this.inputFilter = element }} onChange={this.filterCryptoList} type="text" placeholder="Filter" />
+                <CryptoList cryptoList={this.state.filteredCryptoList} />
+            </div>
+        );
+    }
+}
+
+export default Crypto;
